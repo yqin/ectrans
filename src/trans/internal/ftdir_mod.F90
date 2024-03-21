470df935d1a7ba2fcfea0feb1f95df38c06511c7
Yong Qin <yongq@nvidia.com>
2024-03-21 11:21:31 -0700

    Added timers to profile two main loops that Dmitry is concerned about.

--- src/trans/internal/ftdir_mod.F90
+++ src/trans/internal/ftdir_mod.F90
@@ -65,7 +65,7 @@ REAL(KIND=JPRB), INTENT(INOUT) :: PREEL(:,:)
 
 INTEGER(KIND=JPIM) :: IGLG,IST,ILEN,IJUMP,JJ,JF,IST1
 INTEGER(KIND=JPIM) :: IOFF,IRLEN,ICLEN, ITYPE
-LOGICAL :: LL_ALL=.FALSE. ! T=do kfields ffts in one batch, F=do kfields ffts one at a time
+LOGICAL :: LL_ALL=.TRUE. ! T=do kfields ffts in one batch, F=do kfields ffts one at a time
 
 !     ------------------------------------------------------------------
 
